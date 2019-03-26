1cdef85497b7f462cdb6ae2421ad1db93f32331b
xieguanwen <xieguanwen@gmail.com>
2019-03-26 16:47:24 +0800

    cnn

---
+++ nn/cnn.py
@@ -0,0 +1,95 @@
+import tensorflow as tf,os
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataPath = os.path.join('..','data/mnist/')
+mnist = read_data_sets(dataPath, one_hot=True)
+
+n_input  = 784
+n_output = 10
+weights  = {
+        'wc1': tf.Variable(tf.random_normal([3, 3, 1, 64], stddev=0.1)),
+        'wc2': tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.1)),
+        'wd1': tf.Variable(tf.random_normal([7*7*128, 1024], stddev=0.1)),
+        'wd2': tf.Variable(tf.random_normal([1024, n_output], stddev=0.1))
+    }
+biases   = {
+        'bc1': tf.Variable(tf.random_normal([64], stddev=0.1)),
+        'bc2': tf.Variable(tf.random_normal([128], stddev=0.1)),
+        'bd1': tf.Variable(tf.random_normal([1024], stddev=0.1)),
+        'bd2': tf.Variable(tf.random_normal([n_output], stddev=0.1))
+    }
+
+
+def conv_basic(_input, _w, _b, _keepratio):
+    # INPUT
+    _input_r = tf.reshape(_input, shape=[-1, 28, 28, 1])
+    # CONV LAYER 1
+    _conv1 = tf.nn.conv2d(_input_r, _w['wc1'], strides=[1, 1, 1, 1], padding='SAME')
+    _conv1 = tf.nn.relu(tf.nn.bias_add(_conv1, _b['bc1']))
+    _pool1 = tf.nn.max_pool(_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    _pool_dr1 = tf.nn.dropout(_pool1, _keepratio) # random去掉一些连接
+    # CONV LAYER 2
+    _conv2 = tf.nn.conv2d(_pool_dr1, _w['wc2'], strides=[1, 1, 1, 1], padding='SAME')
+    _conv2 = tf.nn.relu(tf.nn.bias_add(_conv2, _b['bc2']))
+    _pool2 = tf.nn.max_pool(_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    _pool_dr2 = tf.nn.dropout(_pool2, _keepratio)
+    # VECTORIZE
+    _dense1 = tf.reshape(_pool_dr2, [-1, _w['wd1'].get_shape().as_list()[0]]) #学习一下这个操作
+    # FULLY CONNECTED LAYER 1
+    _fc1 = tf.nn.relu(tf.add(tf.matmul(_dense1, _w['wd1']), _b['bd1']))
+    _fc_dr1 = tf.nn.dropout(_fc1, _keepratio)
+    # FULLY CONNECTED LAYER 2
+    _out = tf.add(tf.matmul(_fc_dr1, _w['wd2']), _b['bd2'])
+    # RETURN
+    out = {'input_r': _input_r, 'conv1': _conv1, 'pool1': _pool1, 'pool1_dr1': _pool_dr1,
+           'conv2': _conv2, 'pool2': _pool2, 'pool_dr2': _pool_dr2, 'dense1': _dense1,
+           'fc1': _fc1, 'fc_dr1': _fc_dr1, 'out': _out
+           }
+    return out
+
+x = tf.placeholder(tf.float32, [None, n_input])
+y = tf.placeholder(tf.float32, [None, n_output])
+keepratio = tf.placeholder(tf.float32)
+
+# FUNCTIONS
+
+_pred = conv_basic(x, weights, biases, keepratio)['out']
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(_pred, y))
+optm = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+_corr = tf.equal(tf.argmax(_pred,1), tf.argmax(y,1))
+accr = tf.reduce_mean(tf.cast(_corr, tf.float32))
+init = tf.global_variables_initializer()
+
+
+training_epochs = 20
+batch_size      = 100
+display_step    = 4
+# LAUNCH THE GRAPH
+sess = tf.Session()
+sess.run(init)
+# OPTIMIZE
+for epoch in range(training_epochs):
+    avg_cost = 0.
+    total_batch = int(mnist.train.num_examples/batch_size)
+    # ITERATION
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feeds = {x: batch_xs, y: batch_ys}
+        sess.run(optm, feed_dict=feeds)
+        avg_cost += sess.run(cost, feed_dict=feeds)
+    avg_cost = avg_cost / total_batch
+    # DISPLAY
+    if (epoch+1) % display_step == 0:
+        print ("Epoch: %03d/%03d cost: %.9f" % (epoch, training_epochs, avg_cost))
+        feeds = {x: batch_xs, y: batch_ys}
+        train_acc = sess.run(accr, feed_dict=feeds)
+        print ("TRAIN ACCURACY: %.3f" % (train_acc))
+        feeds = {x: mnist.test.images, y: mnist.test.labels}
+        test_acc = sess.run(accr, feed_dict=feeds)
+        print ("TEST ACCURACY: %.3f" % (test_acc))
+
+saver = tf.train.Saver()
+saver.save(sess,os.path.join('.','model/cnn/mnist'))
+print ("OPTIMIZATION FINISHED")
